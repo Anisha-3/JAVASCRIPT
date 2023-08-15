9d71a247d656233390c42cbd4572c0c488492bf2
Anisha Asnani <anishaasnani2004@gmail.com>
2023-08-16 03:30:09 +0530

    commit1

---
+++ Basics/index.js
@@ -0,0 +1,20 @@
+// line below is used to print on the console below
+console.log('Namaste Duniya version 2');
+
+let a = true;
+console.log(a);
+
+const number = 12;
+//number = 13;
+
+
+//          Dynamically Typed
+
+let lastName = 'Anisha';
+console.log(lastName);
+
+lastName = 3;
+console.log(lastName);
+
+
+
