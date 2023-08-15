6f913f9781bb678b15dac31f276e154c68501a9b
Anisha Asnani <anishaasnani2004@gmail.com>
2023-08-16 03:38:18 +0530

    commit2

--- Basics/index.js
+++ Basics/index.js
@@ -17,4 +17,61 @@ lastName = 3;
 console.log(lastName);
 
 
+//          if-elseif-else statements
+
+let marks = 12;
+if(marks >= 90) {
+    console.log('A');
+}
+else if(marks >= 80) {
+    console.log('B');
+}
+else if(marks >=70) {
+    console.log('C');
+}
+else if(marks >= 60) {
+    console.log('D');
+}
+else {
+    console.log('E');
+}
+
+
+//          switch statements
+
+let num = 1;
+switch(num) {
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
+
+//          for loop
+
+console.log("For Loop");
+for(let i = 1; i<=5; i=i+1) {
+    console.log("Hello " + i);
+}
+
+//          while loop
+
+console.log("While Loop");
+let x = 1;
+while(x <= 5) {
+    console.log(x);
+    x++;
+}
+
+//          do-while loop
+
+console.log("Do-While Loop");
+let y = 1;
+do {
+    console.log(y);
+    y++;
+} while(y < 10);
 
